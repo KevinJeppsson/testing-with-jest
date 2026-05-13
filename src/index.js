027cd97678e41c508bfe8b020ca321c3ee7d1042
KevinJeppsson <kevinkristerjeppsson@gmail.com>
2026-05-13 22:34:26 +0200

    Innan jag gör egna tester

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
-const _ = require("underscore");
+import * as stack from './stack.js';
 
-console.log("Webpack funkar!");
-console.log(_.shuffle([1, 2, 3, 4, 5]));
+window.onload = function () {
+    console.log("done");
+
+    var pop = document.getElementById('pop');
+    var push = document.getElementById('push');
+    var peek = document.getElementById('peek');
+    var display = document.getElementById('top_of_stack');
+
+    pop.addEventListener("click", function() {
+        var text = "Tog bort " + stack.pop();
+        alert(text);
+    });
+
+    push.addEventListener("click", function() {
+        var x = prompt("Vad ska vi lägga på stacken?");
+        stack.push(x);
+        display.innerHTML = x;
+    });
+
+    peek.addEventListener("click", function() {
+        display.innerHTML = stack.peek();
+    });
+};
